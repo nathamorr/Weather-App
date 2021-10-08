d874a53814fdff08f5d3e0d59acad06d7ee3640e
nathan.amorrich <namorric@tlu.ee>
2021-10-08 23:25:35 +0300

    Design v2 home page + API integration + redirection to city_page + Design v1 city page + API integration on city page

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-var button = document.querySelector('.Submit')
-var inputValue = document.querySelector('.InputValue')
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+let weather = {
+  API_Key : "d78f4606c5ba94c8b3d176f95d59163a",
+  fetchWeather: function(city_searched) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q='
+      +city_searched
+      +'&units=metric&appid='
+      +this.API_Key
 
-button.addEventListener('click',function(){
-    fetch('api.openweathermap.org/data/2.5/weather?q={new york}&appid={d78f4606c5ba94c8b3d176f95d59163a}')
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data['name'];
-        var descValue = data['weather'][0]['description'];
-        var tempValue = data['main']['temp'];
+    )
+    .then((response) => response.json())
+    .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data){
+    const{name} = data;
+    const{icon, description} = data.weather[0];
+    const{temp, humidity} = data.main;
+    const{speed} = data.wind;
+    console.log(name,icon,description,temp,humidity,speed);
 
-        name.innerHTML = nameValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
-        
-    })
-    .catch(err => alert("Wront location name"))
+    if(name == "Tallinn" || name == "Paris" || name == "Berlin"){
+      //document.querySelector(".icon_"+name).src = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+      document.querySelector(".desc_"+name).innerHTML = description;
+      document.querySelector(".temp_"+name).innerHTML = temp+"°C";
+      document.querySelector(".humidity_"+name).innerHTML = "Humidity : "+humidity+"%";
+      document.querySelector(".wind_"+name).innerHTML = "Wind speed: "+speed+"km/h";
+    }
+    else{
+    }
+
+  },
+  search : function(){
+    localStorage.setItem('city_name', document.querySelector(".InputValue").value);
+  },
+  
+  redirect : function(){
+    window.location = "http://127.0.0.1:5501/city_page.html";
+  },
+};
+
+window.onload = function() {
+  weather.fetchWeather("Tallinn");
+  weather.fetchWeather("Paris");
+  weather.fetchWeather("Berlin");
+};
+
+document.querySelector(".researchBlock button").addEventListener("click", function(){
+  weather.search();
+  weather.redirect();
+});
+
+
+
+/*
+.catch(err => alert("Wrong city name!"));
 })
-
+*/
---
+++ script_city_page.js
@@ -0,0 +1,40 @@
+var city_name = localStorage.getItem('city_name');
+localStorage.removeItem('city_name');
+alert(city_name)
+
+document.querySelector(".title").innerHTML = "Weather in "+city_name;
+document.querySelector(".titleBlock").innerHTML = "Weather in "+city_name;
+document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+city_name+"')";
+
+let weather = {
+    API_Key : "d78f4606c5ba94c8b3d176f95d59163a",
+    fetchWeather: function(city_searched) {
+      fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q='
+        +city_searched
+        +'&units=metric&appid='
+        +this.API_Key
+  
+      )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+      const{name} = data;
+      const{icon, description} = data.weather[0];
+      const{temp, humidity} = data.main;
+      const{speed} = data.wind;
+      console.log(name,icon,description,temp,humidity,speed);
+    
+      //document.querySelector(".icon_"+name).src = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+      document.querySelector(".name").innerHTML = name;
+      document.querySelector(".desc").innerHTML = description;
+      document.querySelector(".temp").innerHTML = temp+"°C";
+      document.querySelector(".humidity").innerHTML = "Humidity : "+humidity+"%";
+      document.querySelector(".wind").innerHTML = "Wind speed: "+speed+"km/h";
+    },
+};
+
+window.onload = function() {
+    weather.fetchWeather(city_name);
+  };
